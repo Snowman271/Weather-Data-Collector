7a30222e22fbef65bff4e7e1d985f70b0dbe6e1e
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-06 03:18:45 -0800

    Added basic hmtl with some css for containers

---
+++ script.js
@@ -0,0 +1,11 @@
+// var search = document.querySelector(.search);
+
+// fetch(requestUrl)
+// .then(function (response) {
+//   return response.json();
+// })
+// .then(function (data) {
+//   for (var i = 0; i < data.length; i++) {
+//    Text.content(search)
+//   }
+// });
