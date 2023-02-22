333cebe5f3752b7142112756850b707c89685d35
Snowman271 <deanbenzmiller@yahoo.com>
2023-02-21 16:49:09 -0800

    Adding Working Search Bar with Api Calls

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-// var search = document.querySelector(.search);
+var inputEl = document.querySelector(".searchInput");
 
-// fetch(requestUrl)
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//   for (var i = 0; i < data.length; i++) {
-//    Text.content(search)
-//   }
-// });
+
+document.querySelector("button").addEventListener("click", function () {
+    var input = $(".searchInput").val();
+    searchCity(input);
+});
+
+function searchCity(input) {
+var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + input + "&appid=7ddf229751781b7f4cbed1adfe8e2b25";
+fetch(requestUrl)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  console.log(data);
+});
+}
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+// 7ddf229751781b7f4cbed1adfe8e2b25
+
